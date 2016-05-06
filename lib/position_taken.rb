7e8cc54f4d10c094d97eba189e5d92cef249cca5
Joseph Lim <joseph.d.lim@gmail.com>
2016-05-06 14:36:47 -0400

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,9 @@
 # code your #position_taken? method here!
+def position_taken?(board, position)
+  query = board[position]
+  if query == "" || query ==" " || query == nil
+    false
+  else
+    true
+  end
+end
